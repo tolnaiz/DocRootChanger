f25552656698a4d7db412ad332cf4e9a7a8ff9ef
Tolnai Zoltán <tolnai.zoltan@ferling.hu>
2012-03-17 21:39:36 +0100

    1.0

--- LocalDocRootChanger/LDRCAppDelegate.m
+++ LocalDocRootChanger/LDRCAppDelegate.m
@@ -1,20 +1,91 @@
+// Copyright (C) 2012 Zoltan Tolnai
 //
-//  LVSAppDelegate.m
-//  LocalVhostSettings
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+// documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+// to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 //
-//  Created by Zoltán Tolnai on 2012.03.17..
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+// the Software.
 //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
 
-#import "LVSAppDelegate.h"
+#import "LDRCAppDelegate.h"
 
-@implementation LVSAppDelegate
+@implementation LDRCAppDelegate
 
 @synthesize window = _window;
 @synthesize persistentStoreCoordinator = __persistentStoreCoordinator;
 @synthesize managedObjectModel = __managedObjectModel;
 @synthesize managedObjectContext = __managedObjectContext;
 @synthesize statusMenu = _statusMenu;
+
+- (NSMenu *) statusMenu{
+	if( ! _statusMenu) {
+		_statusMenu = [[NSMenu alloc] initWithTitle:@""];
+	}
+	return _statusMenu;
+}
+
+
+
+- (IBAction)quit:(id)sender{
+    [[NSApplication sharedApplication] terminate:self];
+}
+
+- (void)menuWillOpen:(NSMenu *)menu {
+	[statusItem setImage:[NSImage imageNamed:@"icon-w.png"]];
+	[self.statusMenu removeAllItems];
+	NSString *dir = [@"~/Dropbox/Munka/" stringByExpandingTildeInPath];
+	NSFileManager *fm = [NSFileManager defaultManager];
+
+	NSString *currentSymlink = @"";
+	if([fm isReadableFileAtPath:[@"~/Sites/htdocs" stringByExpandingTildeInPath]]) {
+		currentSymlink = [[@"~/Sites/htdocs" stringByResolvingSymlinksInPath] lastPathComponent];
+	}
+
+	BOOL isDir;
+	int i = 0;
+	NSArray *dirContents = [fm contentsOfDirectoryAtPath:dir error:nil];
+	for (NSString *f in dirContents) {
+		NSString *keyeq = @"";
+		if (i<10) {
+			keyeq = [NSString stringWithFormat:@"%d",i];
+		}
+
+		NSString *file = [[dir stringByAppendingString:@"/"] stringByAppendingString:f];
+		if ([fm fileExistsAtPath:file isDirectory:&isDir] && isDir) {
+			NSMenuItem *menuItem = [[NSMenuItem alloc] initWithTitle:f action:@selector(changeVhost:) keyEquivalent:keyeq];
+			if([f isEqualToString:currentSymlink]) {
+				menuItem.state = YES;
+			}
+			[self.statusMenu insertItem:menuItem atIndex: i-1];
+		}
+		i++;
+	}
+	[self.statusMenu insertItem:[NSMenuItem separatorItem] atIndex:i-1];
+	NSMenuItem *quitMenu = [[NSMenuItem alloc] initWithTitle:@"Quit" action:@selector(quit:) keyEquivalent:@"q"];
+	[self.statusMenu insertItem:quitMenu atIndex:i];
+}
+
+- (void)menuDidClose:(NSMenu *)menu{
+    [statusItem setImage:[NSImage imageNamed:@"icon.png"]];
+}
+
+- (void)changeVhost:(NSMenuItem *)sender{
+	NSFileManager *fm = [NSFileManager defaultManager];
+//	NSError *error = [[NSError alloc] init];
+
+	[fm removeItemAtPath: [@"~/Sites/htdocs" stringByExpandingTildeInPath] error: nil];
+	NSString *dir = [@"~/Dropbox/Munka/" stringByExpandingTildeInPath];
+	[fm createSymbolicLinkAtPath:[@"~/Sites/htdocs" stringByExpandingTildeInPath] withDestinationPath:[[dir stringByAppendingString:@"/"] stringByAppendingString:sender.title] error:nil];
+//	NSLog(@"%@",[error localizedDescription]);
+}
+
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
@@ -23,16 +94,16 @@
 	statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
 	[statusItem setMenu:self.statusMenu];
 	[statusItem setHighlightMode:YES];
-	[statusItem setImage:[NSImage imageNamed:@"i.png"]];
-	
+	[statusItem setImage:[NSImage imageNamed:@"icon.png"]];
+
 }
 
-// Returns the directory the application uses to store the Core Data store file. This code uses a directory named "tony.LocalVhostSettings" in the user's Application Support directory.
+// Returns the directory the application uses to store the Core Data store file. This code uses a directory named "tony.LocalDocRootChanger" in the user's Application Support directory.
 - (NSURL *)applicationFilesDirectory
 {
     NSFileManager *fileManager = [NSFileManager defaultManager];
     NSURL *appSupportURL = [[fileManager URLsForDirectory:NSApplicationSupportDirectory inDomains:NSUserDomainMask] lastObject];
-    return [appSupportURL URLByAppendingPathComponent:@"tony.LocalVhostSettings"];
+    return [appSupportURL URLByAppendingPathComponent:@"tony.LocalDocRootChanger"];
 }
 
 // Creates if necessary and returns the managed object model for the application.
@@ -41,8 +112,8 @@
     if (__managedObjectModel) {
         return __managedObjectModel;
     }
-	
-    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"LocalVhostSettings" withExtension:@"momd"];
+
+    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"LocalDocRootChanger" withExtension:@"momd"];
     __managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
     return __managedObjectModel;
 }
@@ -53,19 +124,19 @@
     if (__persistentStoreCoordinator) {
         return __persistentStoreCoordinator;
     }
-    
+
     NSManagedObjectModel *mom = [self managedObjectModel];
     if (!mom) {
         NSLog(@"%@:%@ No model to generate a store from", [self class], NSStringFromSelector(_cmd));
         return nil;
     }
-    
+
     NSFileManager *fileManager = [NSFileManager defaultManager];
     NSURL *applicationFilesDirectory = [self applicationFilesDirectory];
     NSError *error = nil;
-    
+
     NSDictionary *properties = [applicationFilesDirectory resourceValuesForKeys:[NSArray arrayWithObject:NSURLIsDirectoryKey] error:&error];
-    
+
     if (!properties) {
         BOOL ok = NO;
         if ([error code] == NSFileReadNoSuchFileError) {
@@ -79,34 +150,34 @@
         if (![[properties objectForKey:NSURLIsDirectoryKey] boolValue]) {
             // Customize and localize this error.
             NSString *failureDescription = [NSString stringWithFormat:@"Expected a folder to store application data, found a file (%@).", [applicationFilesDirectory path]];
-            
+
             NSMutableDictionary *dict = [NSMutableDictionary dictionary];
             [dict setValue:failureDescription forKey:NSLocalizedDescriptionKey];
             error = [NSError errorWithDomain:@"YOUR_ERROR_DOMAIN" code:101 userInfo:dict];
-            
+
             [[NSApplication sharedApplication] presentError:error];
             return nil;
         }
     }
-    
-    NSURL *url = [applicationFilesDirectory URLByAppendingPathComponent:@"LocalVhostSettings.storedata"];
+
+    NSURL *url = [applicationFilesDirectory URLByAppendingPathComponent:@"LocalDocRootChanger.storedata"];
     NSPersistentStoreCoordinator *coordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:mom];
     if (![coordinator addPersistentStoreWithType:NSXMLStoreType configuration:nil URL:url options:nil error:&error]) {
         [[NSApplication sharedApplication] presentError:error];
         return nil;
     }
     __persistentStoreCoordinator = coordinator;
-    
+
     return __persistentStoreCoordinator;
 }
 
-// Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) 
+// Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.)
 - (NSManagedObjectContext *)managedObjectContext
 {
     if (__managedObjectContext) {
         return __managedObjectContext;
     }
-    
+
     NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
     if (!coordinator) {
         NSMutableDictionary *dict = [NSMutableDictionary dictionary];
@@ -132,11 +203,11 @@
 - (IBAction)saveAction:(id)sender
 {
     NSError *error = nil;
-    
+
     if (![[self managedObjectContext] commitEditing]) {
         NSLog(@"%@:%@ unable to commit editing before saving", [self class], NSStringFromSelector(_cmd));
     }
-    
+
     if (![[self managedObjectContext] save:&error]) {
         [[NSApplication sharedApplication] presentError:error];
     }
@@ -145,24 +216,24 @@
 - (NSApplicationTerminateReply)applicationShouldTerminate:(NSApplication *)sender
 {
     // Save changes in the application's managed object context before the application terminates.
-    
+
     if (!__managedObjectContext) {
         return NSTerminateNow;
     }
-    
+
     if (![[self managedObjectContext] commitEditing]) {
         NSLog(@"%@:%@ unable to commit editing to terminate", [self class], NSStringFromSelector(_cmd));
         return NSTerminateCancel;
     }
-    
+
     if (![[self managedObjectContext] hasChanges]) {
         return NSTerminateNow;
     }
-    
+
     NSError *error = nil;
     if (![[self managedObjectContext] save:&error]) {
 
-        // Customize this code block to include application-specific recovery steps.              
+        // Customize this code block to include application-specific recovery steps.
         BOOL result = [sender presentError:error];
         if (result) {
             return NSTerminateCancel;
@@ -179,7 +250,7 @@
         [alert addButtonWithTitle:cancelButton];
 
         NSInteger answer = [alert runModal];
-        
+
         if (answer == NSAlertAlternateReturn) {
             return NSTerminateCancel;
         }
